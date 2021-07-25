59e0751f96c08c866e34bd8be2797c418aef7223
siddharthgaur2590 <63536923+siddharthgaur2590@users.noreply.github.com>
2021-07-26 00:44:55 +0530

    Add files via upload

---
+++ todo-app/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+
+  /**
+   * External source url to fetch the todo items.
+   */
+  todoSourceUrl: 'https://jsonplaceholder.typicode.com/todos',
+};
